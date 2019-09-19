93f16c65928feb6c27fc6ce1aaf6f70fc0678c0b
FSIDDIQUI <fsiddiqui@ets.org>
2019-09-19 15:57:10 -0400

    footer case fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 
 import Amplify from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react";
