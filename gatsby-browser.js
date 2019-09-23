d668b9a1d5a50578e59aef0ae35c01281ecd0b2f
fsids <fsids3@gmail.com>
2019-09-23 23:00:23 +0000

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,19 +6,6 @@
 
 // You can delete this file if you're not using it
 
-import Auth from '@aws-amplify/auth'
-import { setUser } from './src/utils/auth'
-
-export const onRouteUpdate = (state, page, pages) => {
-  Auth.currentAuthenticatedUser()
-    .then(user => {
-      const userInfo = {
-        ...user.attributes,
-        username: user.username
-      }
-      setUser(userInfo)
-    })
-    .catch(err => {
-      window.localStorage.setItem('gatsbyUser', null)
-    })
-}
+import Amplify, { Auth } from 'aws-amplify'
+import awsConfig from './src/aws-exports'
+Amplify.configure(awsConfig)
