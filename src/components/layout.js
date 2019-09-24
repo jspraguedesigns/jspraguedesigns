a0ffafbe018290665229c287f9bc7290660e5461
fsids <fsids3@gmail.com>
2019-09-24 00:07:57 +0000

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "./header"
 import Footer from "./footer"
 
-//import Amplify from "aws-amplify";
-//import { withAuthenticator } from "aws-amplify-react";
-//import config from "../aws-exports";
-//Amplify.configure(config);
 
-const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+const Layout = props => (
 
-const Layout = ({ children }) => (
+  <div>
     <Header/>
-    {children}
+
+    {props.children}
+
+
     <Footer/>
 
+  </div>
 )
 
-//export default withAuthenticator(Layout, true);
 export default Layout
