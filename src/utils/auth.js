62aa659d71df213eb3a73b4b0902a30f55fef14c
FSIDDIQUI <fsiddiqui@ets.org>
2019-09-27 18:36:13 -0400

    add auth0

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,26 +1,104 @@
-const isBrowser = typeof window !== `undefined`
+import auth0js from 'auth0-js';
 
-export const setUser = user => (window.localStorage.gatsbyUser = JSON.stringify(user))
+export const isBrowser = typeof window !== 'undefined';
 
-const getUser = () => {
-  if (window.localStorage.gatsbyUser) {
-    let user = JSON.parse(window.localStorage.gatsbyUser)
-    return user ? user : {}
+// To speed things up, we’ll keep the profile stored unless the user logs out.
+// This prevents a flicker while the HTTP round-trip completes.
+let profile = false;
+
+const tokens = {
+  accessToken: false,
+  idToken: false,
+  expiresAt: false
+};
+
+// Only instantiate Auth0 if we’re in the browser.
+const auth0 = isBrowser
+  ? new auth0js.WebAuth({
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENTID,
+      redirectUri: process.env.AUTH0_CALLBACK,
+      audience: process.env.AUTH0_AUDIENCE,
+      responseType: 'token id_token',
+      scope: 'openid profile email'
+    })
+  : {};
+
+export const login = () => {
+  if (!isBrowser) {
+    return;
   }
-  return {}
-}
 
-export const isLoggedIn = () => {
-  if (!isBrowser) return false
+  auth0.authorize();
+};
 
-  const user = getUser()
-  if (user) return !!user.username
-}
+export const logout = () => {
+  localStorage.setItem('isLoggedIn', false);
+  profile = false;
 
-export const getCurrentUser = () => isBrowser && getUser()
+  const { protocol, host } = window.location;
+  const returnTo = `${protocol}//${host}`;
 
-export const logout = callback => {
-  if (!isBrowser) return
-  setUser({})
-  callback()
-}
+  auth0.logout({ returnTo });
+};
+
+const setSession = callback => (err, authResult) => {
+  if (!isBrowser) {
+    return;
+  }
+
+  if (err) {
+    console.error(err);
+    callback();
+    return;
+  }
+
+  if (authResult && authResult.accessToken && authResult.idToken) {
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
+    tokens.accessToken = authResult.accessToken;
+    tokens.idToken = authResult.idToken;
+    tokens.expiresAt = expiresAt;
+    profile = authResult.idTokenPayload;
+    localStorage.setItem('isLoggedIn', true);
+    callback();
+  }
+};
+
+export const silentAuth = callback => {
+  if (!isBrowser) {
+    return;
+  }
+
+  if (!isAuthenticated()) return callback();
+  auth0.checkSession({}, setSession(callback));
+};
+
+export const handleAuthentication = (callback = () => {}) => {
+  if (!isBrowser) {
+    return;
+  }
+
+  auth0.parseHash(setSession(callback));
+};
+
+export const isAuthenticated = () => {
+  if (!isBrowser) {
+    return;
+  }
+
+  return localStorage.getItem('isLoggedIn') === 'true';
+};
+
+export const getAccessToken = () => {
+  if (!isBrowser) {
+    return '';
+  }
+
+  return tokens.accessToken;
+};
+
+export const getUserInfo = () => {
+  if (profile) {
+    return profile;
+  }
+};
