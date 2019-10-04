64c3e2600dce0b459d31bb9f19bbd780af539ce6
FSIDDIQUI <fsiddiqui@ets.org>
2019-10-04 12:49:26 -0400

    update gatsby-node. delete account.js
    Signed-off-by: FSIDDIQUI <fsiddiqui@ets.org>

--- src/components/sample.js
+++ src/components/sample.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
 
-
 import 'jquery/dist/jquery.js'
 
 const Sample = () => {
     return (
         <div>
         <h1>Sample Questions</h1>
-        <div class="row">
+            <div className="row">
   <div class="col-md-12 pt-4 pb-4 text-center">
   <strong>Directions:</strong> Fill in the missing letters. For each blank, type one letter.
   </div>
@@ -22,6 +21,6 @@ const Sample = () => {
 
     )
 
-}
+};
 
 export default Sample
