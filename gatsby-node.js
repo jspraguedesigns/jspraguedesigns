437833f277a4ed249f51202a655d1e3adae7bf38
FSIDDIQUI <fsiddiqui@ets.org>
2019-10-04 12:48:22 -0400

    update gatsby-node. delete account.js
    Signed-off-by: FSIDDIQUI <fsiddiqui@ets.org>

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,73 +6,17 @@
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
 exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
+    const {createPage} = actions;
 
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
   if (page.path.match(/^\/app/)) {
-    page.matchPath = "/app/*"
+      page.matchPath = "/app/*";
 
     // Update the page.
     createPage(page)
   }
-
-  if (page.path.match(/^\/account/)) {
-    page.matchPath = "/account/*"
-
-    // Update the page.
-    createPage(page)
-  }
-
-}
-
-exports.createPages = ({ actions }) => {
-    const { createPage } = actions
-    // pull in or use whatever data
-    const studentsData = [
-      {
-        name: "Wang, Jing",
-        id: "001",
-        score: "145",
-        date: "2019-10-20",
-        dob: "2005-07-22",
-        grade: "10",
-        performance: "Students at this level typically understand little in some everyday English, both spoken and written, but would like to feel more confident in using English to communicate. Their knowledge of English vocabulary and grammatical structures could be strengthened so that they can understand complex, academic content.",
-      },
-      {
-        name: "Yuxi, Yao",
-        id: "002",
-        score: "140",
-        date: "2019-10-20",
-        dob: "2006-02-23",
-        grade: "9",
-        performance: "Students at this level typically understand little in some everyday English, both spoken and written, but would like to feel more confident in using English to communicate. Their knowledge of English vocabulary and grammatical structures could be strengthened so that they can understand complex, academic content.",
-      },
-    ]
-
-
-  const classesData = [
-    { names: "EnglishLevel1", score: "90", teacher: "Mrs. Smith" },
-    { names: "EnglishLevel2", score: "97", teacher: "Mrs. Smith" },
-    { names: "EnglishLevel3", score: "124", teacher: "Mrs. Adams" },
-  ]
-
-  studentsData.forEach(students => {
-        createPage({
-          path: `/app/${students.name}`,
-          component: require.resolve(`./src/templates/student-template.js`),
-          context: { students },
-        })
-      })
-
-  classesData.forEach(cls => {
-    createPage({
-      path: `/app/${cls.name}`,
-      component: require.resolve(`./src/templates/class-template.js`),
-      context: { cls },
-    })
-  })
-}
+};
 
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html") {
@@ -94,4 +38,4 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       },
     })
   }
-}
+};
