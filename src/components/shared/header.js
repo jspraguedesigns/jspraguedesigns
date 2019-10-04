39417021763276ffcb85f9e91e6ae9f33b5d6a9b
FSIDDIQUI <fsiddiqui@ets.org>
2019-10-04 09:51:20 -0400

    switch callback url in env.prod
    Signed-off-by: FSIDDIQUI <fsiddiqui@ets.org>

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -1,82 +1,77 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Nav, Navbar, NavDropdown } from "react-bootstrap"
+import {Nav, Navbar, NavDropdown} from "react-bootstrap"
 import "../../styles/global.css"
-import { getUserInfo, isAuthenticated, login, logout } from "../../utils/auth"
+import {getUserInfo, isAuthenticated, login, logout} from "../../utils/auth"
 
 const Header = () => {
   if (!isAuthenticated()) {
-    login()
+      login();
     return <p>Redirecting to login...</p>
-  } else  {
- const user = getUserInfo()
+  } else {
+      const user = getUserInfo();
     return (
-<div>
-      <Navbar bg="light" expand="md">
-        <Navbar.Brand href="/">
+        <div>
+            <Navbar bg="light" expand="md">
+                <Navbar.Brand href="/">
+                    <div className="navbar-brand  d-flex align-items-center">
+                        <img
+                            src="https://www.ets.org/rsc/img/ets-logo_68x46.png"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="ETS logo"
+                        />
+                        <span className="brand">ELC</span>
+                        <span className="tag">— Admin Portal</span>
+                    </div>
+                </Navbar.Brand>
 
-          <div className="navbar-brand  d-flex align-items-center">
-            <h1 className="brand">ELC</h1>
-            <h2 className="tag">— Admin Portal</h2>
-          </div>
-          {/*<div> Welcome, {user.name ? user.name : "friend"}! </div>*/}
-        </Navbar.Brand>
+                <Nav className="ml-auto">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse>
+                        <NavDropdown title="ELC Overview" id={"4628376526"}>
+                            <NavDropdown.Item href="/app/about">
+                                What Is ELC?
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/app/sample">
+                                Sample Questions
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Teacher's Resources" id={"0807598482"}>
+                            <NavDropdown.Item href="/app/support">
+                                Support Materials
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/app/teacher_training">
+                                Teacher Training
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/app/proctoring">
+                                Proctoring
+                            </NavDropdown.Item>
+                        </NavDropdown>
 
+                        <Nav.Link href="/app/test-results">
+                            Test Report <small>(Beta)</small>
+                        </Nav.Link>
 
-        <Nav className="ml-auto">
-
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse>
-            <Nav.Link href="/">
-              Home
-            </Nav.Link>
-            <NavDropdown title="ELC Overview" id={"4628376526"}>
-
-              <NavDropdown.Item href="/app/about">
-                What Is ELC?
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/app/sample">
-                Sample Questions
-              </NavDropdown.Item>
-
-
-          </NavDropdown>
-            <NavDropdown title="Teacher's Resources" id={"0807598482"}>
-              <NavDropdown.Item href="/app/support">
-                Support Materials
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/app/teacher_training">
-                Teacher Training
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/app/proctoring">
-                Proctoring
-
-              </NavDropdown.Item>
-
-            </NavDropdown>
-
-            <Nav.Link href="/app/test-results">
-              Test Report <small>(Beta)</small>
-            </Nav.Link>
-
-            <Nav.Link
-              href="#logout"
-              onClick={e => {
-                logout()
-                e.preventDefault()
-              }}
-            >
-              Log Out
-            </Nav.Link>
-          </Navbar.Collapse>
-        </Nav>
-
-
-      </Navbar>
-
-</div>
+                        <Nav.Link
+                            href="#logout"
+                            onClick={e => {
+                                logout();
+                                e.preventDefault()
+                            }}
+                        >
+                            Log Out
+                        </Nav.Link>
+                        <div className="navbar-text text-wrap font-italic">
+                            {user.name ? user.name : "friend"}
+                        </div>
+                    </Navbar.Collapse>
+                </Nav>
+            </Navbar>
+        </div>
     )
   }
-}
+};
 
 export default Header
--- src/components/shared/headerPublic.js
+++ src/components/shared/headerPublic.js
@@ -1,26 +1,24 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Nav, Navbar, NavDropdown } from "react-bootstrap"
 import "../../styles/global.css"
-import { getUserInfo, isAuthenticated, login, logout } from "../../utils/auth"
 
-const HeaderPublic = (props) => {
-
+const HeaderPublic = props => {
     return (
-<div>
-      <Navbar bg="light" expand="md">
-        <Navbar.Brand href="/">
-
-          <div className="navbar-brand  d-flex align-items-center">
-            <h1 className="brand">ELC</h1>
-            <h2 className="tag"></h2>
-          </div>
-        </Navbar.Brand>
-      </Navbar>
-
-</div>
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <a className="navbar-brand" href="/">
+                    <img
+                        src="https://www.ets.org/rsc/img/ets-logo_68x46.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="ETS logo"
+                    />
+                    <span className="brand">ELC</span>
+                </a>
+            </nav>
+        </div>
     )
-  // }
-}
+};
 
 export default HeaderPublic
