ef2fd8dc026687575c2e144da9635a5f827b372b
fsids <fsids3@gmail.com>
2019-10-06 16:56:51 +0000

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,20 +5,17 @@
  */
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
-exports.onCreatePage = async ({ page, actions }) => {
-    const {createPage} = actions;
-
+export const onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
   if (page.path.match(/^\/app/)) {
-      page.matchPath = "/app/*";
-
+    page.matchPath = "/app/*"
     // Update the page.
     createPage(page)
   }
-};
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+export const onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html") {
     /*
      * During the build step, `auth0-js` will break because it relies on
