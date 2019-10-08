960b37cb015516b6d471090eae9d002d44db5a9e
fsids <fsids3@gmail.com>
2019-10-08 01:57:58 +0000

    Update launch.js

--- src/components/school/launch.js
+++ src/components/school/launch.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "jquery/dist/jquery.js"
-import { osName } from "react-device-detect"
+import { osName, osVersion } from "react-device-detect"
 //import DownloadBlock from "../shared/downloadblock"
 
 const Launch = () => {
