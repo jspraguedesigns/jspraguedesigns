f176f1052cc7f0fb340fc99af8031403f7f88cdf
Jason Sprague <jsprague@ets.org>
2019-10-17 10:38:52 -0400

    launch tweaks

--- src/components/school/launch.js
+++ src/components/school/launch.js
@@ -122,7 +122,7 @@ const Launch = () => {
                   <div className="desktop mr-auto">
            
             <h5>Step 2:</h5>
-                <strong><p>Download the test launch file and save the test launch file to the desktop.</p></strong>
+                <strong><p>Download the test launch file.</p></strong>
               </div>
                      
             </div>
@@ -135,62 +135,64 @@ const Launch = () => {
             </div>
             </div>
             </div>
+            
+            <div className="col-md-12 border d-flex align-items-stretch step">
+              <div className="w-100 p-4 d-flex align-items-center">
+              <div className="instruction">
+                  <div className="desktop mr-auto">
+           
+            <h5>Step 3:</h5>
+                <strong><p>Locate the test launch file in your computer's Download folder and save to the desktop.</p></strong>
+              </div>
+                     
             </div>
+            <div className="link ml-auto">
+            <a
+              href="https://researchtech1.ets.org/c3.net/Falcon/FalconStartProd.seb"
+              className="btn btn-home-top btn btn-secondary"
+            ><strong>Download Test Launch</strong>
+            </a>
+            </div>
+            </div>
+            </div>
+       
 
             <div className="col-md-12 border d-flex align-items-stretch step p-4">
                 <div className="w-100 d-flex align-items-center">
-                <div>
-                <h5>Step 3:</h5>
+                  <div>
+                  <h5>Step 4:</h5>
                     <p><strong>Double click the launch file that you saved to your desktop.<br /> This will launch the practice test.</strong></p>
-                </div>
-                
+                  </div>
                 </div>
                  <div className="icon-box text-center">
                    <img className="icon-pic" src={icon} alt="icon"/>
                    <br/>
                    <small><i>Launch file icon depicted above</i></small>
-                 </div>
-
-               
+                 </div>  
             </div>
             <div className="col-md-12 border d-flex align-items-stretch step p-4">
                 <div className="w-100">
                   <div className="desktop">
-                    <h5>Step 4:</h5>
-                    <p><strong>Enter the practice test login I.D. provided to you by your school’s ELC Assessment Coordinator</strong></p>
-                 
-                 
-                 
-                
+                    <h5>Step 5:</h5>
+                    <p><strong>Enter the practice test login I.D. provided to you by your school’s ELC Assessment Coordinator</strong></p> 
                 </div>
               </div>
             </div>
+
             <div className="col-md-12 border d-flex align-items-stretch step p-4">
-                <div className="w-100">
-                <h5>Step 4:</h5>
-            <p><strong>Download and print: <a target="_blank" href={pdf}>Student Activity Sheet</a> and <a target="_blank" href={pdf2}>Student Problem Report</a></strong></p><br/>
-            </div>
+              <div className="w-100">
+                <h5>Step 6:</h5>
+                  <p><strong>Download and print: <a target="_blank" href={pdf}>Student Activity Sheet</a> and <a target="_blank" href={pdf2}>Student Problem Report</a></strong></p><br/>
+              </div>
             </div>
             <div className="text-center">
-           
-            <p>
-
-<strong>
-  Recommended: Perform trial runs of the practice test using a small group of
-  students to validate that everything is ready
-</strong><br/>
-<Link className="mt-4 mb-4 btn btn-home-top btn btn-secondary" to="app/trouble_shooting">View our Toubleshooting Guide</Link>
-
- 
-</p>
-</div>
+              <p> <strong> Recommended: Perform trial runs of the practice test using a small group of students to validate that everything is ready</strong><br/>
+              <Link className="mt-4 mb-4 btn btn-home-top btn btn-secondary" to="app/trouble_shooting">View our Toubleshooting Guide</Link></p>
             </div>
+            </div>
+</div>
 
-
- 
-          
-
-        <div className="test-day mt-4 mb-4">
+<div className="test-day mt-4 mb-4">
           <div className="row pb-4">
             <div className="col-md-12 text-center">
               <h2>On Test Day</h2>
