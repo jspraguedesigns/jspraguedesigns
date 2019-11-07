107577a17d0e89cb5a9959cf3276ba0aa08f0c3e
Jason Sprague <jsprague@ets.org>
2019-11-07 21:34:42 +0000

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,26 @@
 import React from 'react'
 import {Link} from 'gatsby'
-import wechatimg from "../images/wechat.png"
-import qrcodeimg from "../images/qrcode.jpg"
+import Comments from '../components/comments'
 import {getUserInfo} from "../utils/auth";
 
 const Home = () => {
   return (
 
 <div>
- 
+ <Comments />
   <div className="row d-flex align-items-center justify-content-center  banner">
     <div className="col-md-6 text-center animated fadeInUp">
-
       <h1 className="home-text">English<br/> Language<br/> Certificate</h1>
+      
       <Link to="/app/about">
         <button type="button" className="btn btn-home-top btn btn-secondary mt-4">Learn more about ELC
         </button>
-
+        </Link>
            <Link to="/app/launch">
         <button type="button" className="btn btn-home-top btn btn-secondary mt-4 ml-4">Launch Test
         </button>
       </Link>
-      </Link>
+  
 
     </div>
     <div className="col-md-6">
