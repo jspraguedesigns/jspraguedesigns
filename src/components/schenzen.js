b8a605b32d41a2f632a70dc47cec91c5f4dc5583
Jason Sprague <jsprague@ets.org>
2019-11-15 17:19:04 -0500

    more jiyun edits

--- src/components/schenzen.js
+++ src/components/schenzen.js
@@ -4,6 +4,7 @@ import scores from '../../data/scores.json';
 import { process } from '@progress/kendo-data-query';
 import { Grid, GridColumn } from '@progress/kendo-react-grid';
 import { Window } from '@progress/kendo-react-dialogs';
+import  $ from 'jquery'
 import fig1 from "../images/unnamed-chunk-2-1.png";
 import fig2 from "../images/unnamed-chunk-3-1.png";
 import fig3 from "../images/unnamed-chunk-4-1.png";
@@ -15,6 +16,17 @@ import fig8 from "../images/unnamed-chunk-10-1.png";
 import fig9 from "../images/unnamed-chunk-11-1.png";
 import fig10 from "../images/unnamed-chunk-12-1.png";
 
+function display1 (){
+  $('#id1').fadeIn('fast').siblings().fadeOut('fast');
+}
+
+function display2 (){
+    $('#id2').fadeIn('fast').siblings().fadeOut('fast');
+  }
+
+  function display3(){
+    $('#id3').fadeIn('fast').siblings().fadeOut('fast');
+  }
 
 class Schenzen extends Component {
   constructor(props) {
@@ -59,7 +71,22 @@ class Schenzen extends Component {
   render() {
     return (
       <div>
-      <div className="kendo-react-getting-started">
+
+        <section class="banner-score">
+        <div class="banner_container">
+        <h1 class="banner_title">ELC Entry Results</h1>
+       </div>
+       <div class="section_menu" id="global_menu">
+       <ul>
+         <li class="selected" onClick={display1}><a href="#0" >Individual Level Scores</a></li>
+         <li><a href="#0" onClick={display2}>Total Scores</a></li>
+         <li><a href="#0" onClick={display3}>Individual Scores</a></li>
+</ul>
+</div>
+</section>
+<form>
+  <fieldset id="id1">
+      <div className="container mt-4">
         <h1>Shen Zhen High School:<br/> ELC Entry Individual Level Scores</h1>
         <hr/>
         
@@ -98,13 +125,16 @@ class Schenzen extends Component {
         }
         
       </div>
+      </fieldset>
+      <fieldset id="id2">
+      <div className="container mt-4">
       <div className="row">
         <div className="col-md-12 mt-4">
         <p>Only group size >=25 are illustrated</p>
         <h1>Total Scores</h1>
    
         </div>
-      </div>
+    
 
 
         <div id="all1" className="col-md-12 mt-4">
@@ -136,7 +166,12 @@ class Schenzen extends Component {
         <h3>Compare: between teachers (within-grade)</h3>
         <img className="img-fluid" src={fig5} alt="fig5"/>
         </div>
- 
+        </div>
+        </div>
+ </fieldset>
+ <fieldset id="id3">
+ <div className="container mt-4">
+      <div className="row">
         <h2>Category Scores</h2>
 
         <div id="all2" className="col-md-12">
@@ -170,7 +205,10 @@ class Schenzen extends Component {
         <img className="img-fluid" src={fig10} alt="fig10"/>
         </div>
       </div>  
-  
+      </div>
+      </fieldset>
+  </form>
+ </div>
     );
   }
 }
--- src/components/school/test-results.js
+++ src/components/school/test-results.js
@@ -8,18 +8,18 @@ const TestResults = () => {
   return (
 <div>
 
-      <div className="container">
+     
         <Comments/>
-        <div className="row">
-          <div className="col-md-12 mt-4 mb-4 text-center">
+     
+        
            <Schenzen/>
-          </div>
-        </div>
+      
+      
 
     
 
       </div>
-</div>
+
 
   )
 };
