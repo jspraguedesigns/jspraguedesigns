d2a4f02799bfb48d7e550851bfca4c58c8d3187a
Jason Sprague <jsprague@ets.org>
2019-11-20 17:10:50 -0500

    usability fixes

--- src/components/about.js
+++ src/components/about.js
@@ -15,11 +15,11 @@ const About = () => {
                     <div className="image-what-is-elc">
             </div>
             <p>ELC is a comprehensive assessment system for English language learners around the world.</p>
-
+<br/>
 <p>It is added to / embedded in a language program curriculum and it begins with the <strong>ELC Entry Assessment</strong> that evaluates students’ proficiency at the beginning of a learning cycle and recommends an ELC assessment path.</p>
-
+<br/>
   <p>A series of <strong>ELC leveled assessments</strong> are then administered throughout the learning process so students can document both short-term and long-term progress.</p>
-
+<br/>
   <p>After completing and ELC assessment, schools, teachers, students and parents receive information on performance. This information includes:</p>
   <ul>
     <li>Overall scores</li>
@@ -33,18 +33,18 @@ const About = () => {
 
 
 <p>All of this information is used to inform students, teachers, parents, and program administrators about the students’ strengths and areas that need improvement. It is also used to plan the next round of instruction cycles.</p>
-
+<br/>
 <h3>As A Teacher, What’s My Role?</h3>
 <hr/>
 <div className="image-test">
 </div>
 
 <p>Teachers are at the center of ELC and play key roles throughout the program. They also have a number of opportunities to grow professionally and to use ELC data for increased teaching effectiveness.</p>
-
+<br/>
 <p>Teachers receive <Link to="/app/teacher_training">training</Link> and <Link to="/app/support">support materials</Link> they can use in their classes to teach and monitor students' progress.</p>
-
+<br/>
 <p>Teachers may be asked to administer and proctor both the ELC Entry Assessment and the ELC Leveled Assessments.</p>
-
+<br/>
 <p>Teachers prepare students to take the different ELC assessments using an array of ELC practice and learning materials and they use ELC performance data for feedback and to inform lesson and course planning.</p>
                     
                     </div>
